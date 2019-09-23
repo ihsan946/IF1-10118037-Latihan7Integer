3861d283e369e37541aa5302081ad96cbf04d311
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:52:03 +0700

    Add files via upload

---
+++ IF110118037Latihan7Integer/src/if110118037latihan7integer/IF110118037Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan7integer;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program dengan variabel
+ *                       Integer.
+ */
+public class IF110118037Latihan7Integer {
+
+    
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short integer : " + s);
+        System.out.println("integer  : " + i);
+        System.out.println("long integer : " + l);
+        
+                
+    }
+    
+}
